9b96724c31d986543ce742e6acc783d77fda7594
Atanas Bunchev <atanas.bunchev@gmail.com>
2024-03-10 23:43:20 +0200

    UsersService: Implement UpdateUserAsync

--- SkyHawk.ApplicationServices/Implementation/UsersService.cs
+++ SkyHawk.ApplicationServices/Implementation/UsersService.cs
@@ -43,29 +43,29 @@ public class UsersService : IUsersService
     public async Task<CreateUserResponse> CreateUserAsync(CreateUserRequest request)
     {
         var maxUsernameLength = typeof(User).GetProperty("Username")
-                .GetCustomAttribute<MaxLengthAttribute>().Length;
-        if(request.Username.Length > maxUsernameLength) {
+                ?.GetCustomAttribute<MaxLengthAttribute>()?.Length;
+        if(maxUsernameLength != null && request.Username.Length > maxUsernameLength) {
             return new(BusinessStatusCodeEnum.InvalidInput,
                     $"Username must be less than {maxUsernameLength} symbols long!");
         }
 
         var minUsernameLength = typeof(User).GetProperty("Username")
-                .GetCustomAttribute<MinLengthAttribute>().Length;
-        if(request.Username.Length < minUsernameLength) {
+                ?.GetCustomAttribute<MinLengthAttribute>()?.Length;
+        if(minUsernameLength != null && request.Username.Length < minUsernameLength) {
             return new(BusinessStatusCodeEnum.InvalidInput,
                     $"Username must be at least {minUsernameLength} symbols long!");
         }
 
         var maxPasswordLength = typeof(User).GetProperty("Password")
-                .GetCustomAttribute<MaxLengthAttribute>().Length;
-        if(request.Password.Length > maxPasswordLength) {
+                ?.GetCustomAttribute<MaxLengthAttribute>()?.Length;
+        if(maxPasswordLength != null && request.Password.Length > maxPasswordLength) {
             return new(BusinessStatusCodeEnum.InvalidInput,
                     $"Password must be less than {maxPasswordLength} symbols long!");
         }
 
         var minPasswordLength = typeof(User).GetProperty("Password")
-                .GetCustomAttribute<MinLengthAttribute>().Length;
-        if(request.Password.Length < minPasswordLength) {
+                ?.GetCustomAttribute<MinLengthAttribute>()?.Length;
+        if(minPasswordLength != null && request.Password.Length < minPasswordLength) {
             return new(BusinessStatusCodeEnum.InvalidInput,
                     $"Password must be at least {minPasswordLength} symbols long!");
         }
@@ -89,7 +89,50 @@ public class UsersService : IUsersService
 
     public async Task<UpdateUserResponse> UpdateUserAsync(UpdateUserRequest request)
     {
-        return new(BusinessStatusCodeEnum.NotImplemented, "Not implemented");
+        var user = (await GetUserByIdAsync(new(request.Id))).User;
+        if(user == null)
+            return new(BusinessStatusCodeEnum.NotFound, "User not found!");
+
+
+        if(request.Username != null) {
+            var maxUsernameLength = typeof(User).GetProperty("Username")
+                    ?.GetCustomAttribute<MaxLengthAttribute>()?.Length;
+            if(maxUsernameLength != null && request.Username.Length > maxUsernameLength) {
+                return new(BusinessStatusCodeEnum.InvalidInput,
+                        $"Username must be less than {maxUsernameLength} symbols long!");
+            }
+
+            var minUsernameLength = typeof(User).GetProperty("Username")
+                    ?.GetCustomAttribute<MinLengthAttribute>()?.Length;
+            if(minUsernameLength != null && request.Username.Length < minUsernameLength) {
+                return new(BusinessStatusCodeEnum.InvalidInput,
+                        $"Username must be at least {minUsernameLength} symbols long!");
+            }
+
+            user.Username = request.Username;
+        }
+
+        if(request.Password != null) {
+            var maxPasswordLength = typeof(User).GetProperty("Password")
+                    ?.GetCustomAttribute<MaxLengthAttribute>()?.Length;
+            if(maxPasswordLength != null && request.Password.Length > maxPasswordLength) {
+                return new(BusinessStatusCodeEnum.InvalidInput,
+                        $"Password must be less than {maxPasswordLength} symbols long!");
+            }
+
+            var minPasswordLength = typeof(User).GetProperty("Password")
+                    ?.GetCustomAttribute<MinLengthAttribute>()?.Length;
+            if(minPasswordLength != null && request.Password.Length < minPasswordLength) {
+                return new(BusinessStatusCodeEnum.InvalidInput,
+                        $"Password must be at least {minPasswordLength} symbols long!");
+            }
+
+            user.Password = request.Password;
+        }
+
+        await _context.SaveChangesAsync();
+
+        return new(BusinessStatusCodeEnum.Success, "User updated successfully.");
     }
 
 
