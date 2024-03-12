54e8005e5d0b5e3eaaa9b2a8818e88703e8bdd64
Atanas Bunchev <atanas.bunchev@gmail.com>
2024-03-12 16:24:49 +0200

    WebAPI: Setup Dependency Injection

--- SkyHawk.WebAPI/Program.cs
+++ SkyHawk.WebAPI/Program.cs
@@ -1,11 +1,29 @@
+using Docker.DotNet;
+using SkyHawk.ApplicationServices.Implementation;
+using SkyHawk.ApplicationServices.Interfaces;
+using SkyHawk.Data.Contexts;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<SkyHawkDbContext>();
+builder.Services.AddSingleton<IDockerClient>(serviceProvider => new DockerClientConfiguration().CreateClient());
+
+builder.Services.AddScoped<IUsersService, UsersService>();
+builder.Services.AddScoped<IServersService, ServersService>();
+builder.Services.AddScoped<ISnapshotsService, SnapshotsService>();
 
 var app = builder.Build();
 
