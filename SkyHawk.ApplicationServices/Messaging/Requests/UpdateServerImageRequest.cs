1fa4e4461b919c1482229080ac161f58b5097468
Atanas Bunchev <atanas.bunchev@gmail.com>
2024-03-12 03:35:25 +0200

    ServersServiceTests: Test UpdateServerImage

--- SkyHawk.ApplicationServices/Messaging/Requests/UpdateServerImageRequest.cs
+++ SkyHawk.ApplicationServices/Messaging/Requests/UpdateServerImageRequest.cs
@@ -1,11 +1,16 @@
 using SkyHawk.Data.Entities;
+using System.IO;
 
 namespace SkyHawk.ApplicationServices.Messaging.Requests;
 
 public class UpdateServerImageRequest : ServerRequestBase
 {
-    public UpdateServerImageRequest(User user) : base(user)
+    public int ServerId { get; set; }
+    public Stream Image { get; set; }
+
+    public UpdateServerImageRequest(User user, int serverId, Stream image) : base(user)
     {
-        
+        ServerId = serverId;
+        Image = image;
     }
 };
---
+++ SkyHawk.ApplicationServicesTests/ServersServiceTests/UpdateServerImage.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using SkyHawk.Data.Entities;
+using SkyHawk.ApplicationServices.Messaging;
+
+namespace SkyHawk.ApplicationServicesTests;
+
+public partial class ServersServiceTests
+{
+    [Fact]
+    public async void UpdateServerImage_Succeeds()
+    {
+        var server = GetValidServerEntity();
+        server.Image = null;
+        _context.Servers.Add(server);
+        _context.SaveChanges();
+
+        MemoryStream input = new (100);
+        for(int i = 0; i < 100; i++)
+            input.WriteByte((byte) ((i * 41 + 13) % 97));
+        input.Position = 0;
+
+        var response = await _service.UpdateServerImageAsync(new(_user, server.Id, input));
+        Assert.Equal(BusinessStatusCodeEnum.Success, response.StatusCode);
+        
+        server = await _context.Servers.SingleOrDefaultAsync(x => x.Id == server.Id);
+        Assert.Equal(100, server.Image.Length);
+        for(int i = 0; i < 100; i++) {
+            if((i * 41 + 13) % 97 != server.Image[i]) {
+                Assert.Fail("Blob not stored correctly");
+            }
+        }
+    }
+
+    public async void UpdateServerImage_NotFound_Fails()
+    {
+        MemoryStream input = new (100);
+        for(int i = 0; i < 100; i++)
+            input.WriteByte((byte) ((i * 41 + 13) % 97));
+
+        var response = await _service.UpdateServerImageAsync(new(_user, 10, input));
+        Assert.Equal(BusinessStatusCodeEnum.NotFound, response.StatusCode);
+        Assert.Empty(_context.Servers);
+    }
+}
