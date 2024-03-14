9d1a98bc87d4ab4b0617e491c059981438c02d2d
Atanas Bunchev <atanas.bunchev@gmail.com>
2024-03-14 06:48:22 +0200

    ServersServiceTests: Merge GetServerBy{Id,Name} into GetServer

--- SkyHawk.ApplicationServicesTests/ServersServiceTests/GetServer.cs
+++ SkyHawk.ApplicationServicesTests/ServersServiceTests/GetServer.cs
@@ -6,6 +6,22 @@ namespace SkyHawk.ApplicationServicesTests;
 public partial class ServersServiceTests : IDisposable
 {
     [Fact]
+    public async void TestGetServerById()
+    {
+        var server = GetValidServerEntity();
+
+        var nullResponse = await _service.GetServerByIdAsync(new(_user, 10));
+        Assert.Null(nullResponse.Server);
+
+        _context.Servers.Add(server);
+        _context.SaveChanges();
+
+        var response = await _service.GetServerByIdAsync(new(_user, server.Id));
+
+        Assert.Equal(server, response.Server);
+    }
+
+    [Fact]
     public async void TestGetServerByName()
     {
         var server = GetValidServerEntity();
